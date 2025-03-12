24e80066dd72b375ce8e36ca819709787107dd77
Miika Pihkola <miika.pihkola@student.lab.fi>
2025-03-12 11:12:47 +0200

    HT6: Guess game Buttons created

--- app/src/main/java/com/example/androidjavacourse/Game.java
+++ app/src/main/java/com/example/androidjavacourse/Game.java
@@ -1,6 +1,9 @@
 package com.example.androidjavacourse;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class Game extends AppCompatActivity {
 
+    public static final String TAG ="GuessGame";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,49 @@ public class Game extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Game buttons
+        ImageButton btn1 = (ImageButton) findViewById(R.id.gameBtn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gameBtnPressed(1);
+            }
+        });
+        ImageButton btn2 = (ImageButton) findViewById(R.id.gameBtn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gameBtnPressed(2);
+            }
+        });
+        ImageButton btn3 = (ImageButton) findViewById(R.id.gameBtn3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gameBtnPressed(3);
+            }
+        });
+        ImageButton btn4 = (ImageButton) findViewById(R.id.gameBtn4);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gameBtnPressed(4);
+            }
+        });
+        FloatingActionButton refBtn = (FloatingActionButton) findViewById(R.id.refreshBtn);
+        refBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                refreshGame();
+            }
+        });
+    }
+
+    public void gameBtnPressed(int id){
+        Log.d(TAG, "Button " + id + " clicked");
+    }
+    public void refreshGame(){
+        Log.d(TAG, "RefreshBtn clicked");
     }
 }
