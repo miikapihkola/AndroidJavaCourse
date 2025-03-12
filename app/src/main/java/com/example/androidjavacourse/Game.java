fb0705ed47ef2765e9a8db7ca66025ced41d6f7e
Miika Pihkola <miika.pihkola@student.lab.fi>
2025-03-12 10:16:52 +0200

    HT6: Guess game New activity created Button opens activity

--- app/src/main/java/com/example/androidjavacourse/Game.java
+++ app/src/main/java/com/example/androidjavacourse/Game.java
@@ -1,10 +1,6 @@
 package com.example.androidjavacourse;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,35 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class HelloWorld extends AppCompatActivity {
+public class Game extends AppCompatActivity {
 
-    public static final String TAG ="HelloWorld";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_hello_world);
+        setContentView(R.layout.activity_game);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Log.i(TAG, "Created");
-
-        // Intents
-        Intent guessGame = new Intent(this, Game.class);
-
-        // Start button
-        Button startBtn = (Button) findViewById(R.id.StartBtn);
-        startBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "button clicked");
-                startActivity(guessGame);
-            }
-        });
     }
-
-
-
 }
