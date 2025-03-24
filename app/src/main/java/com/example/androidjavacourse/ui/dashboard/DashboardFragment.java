5b4ecc4f6abdbba490145dd9734ae7d2ef0de32c
Miika Pihkola <miika.pihkola@student.lab.fi>
2025-03-24 15:12:50 +0200

    HT6: Guess game Needs datastoring and change picture to diamond HT8: Laitteen sijainnin hakeminen Get location code done, need implement other stuff

--- app/src/main/java/com/example/androidjavacourse/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/androidjavacourse/ui/dashboard/DashboardFragment.java
@@ -1,19 +1,27 @@
 package com.example.androidjavacourse.ui.dashboard;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.androidjavacourse.databinding.FragmentDashboardBinding;
 
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends Fragment implements LocationListener{
 
+    public static final String TAG ="LocationFrag";
     private FragmentDashboardBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -24,8 +32,9 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        locationMethod();
+
+        // Tämä viimeisenä
         return root;
     }
 
@@ -33,5 +42,28 @@ public class DashboardFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        Log.d(TAG, location.toString());
+    }
+
+    public void locationMethod(){
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        // MUISTA ETTÄ FRAGMENTISSA this MUUTTUU getContext()
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Log.d(TAG,"Location method permission if completed");
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 20000L, (float) 0, (LocationListener) this);
+        Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
     }
 }
